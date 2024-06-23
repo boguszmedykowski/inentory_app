98129242971192450e9bee9eb2c01caffd960014
Bogusz Mędykowski <bogusz.medykowski@gmail.com>
2024-06-23 12:53:38 +0200

    add navigation panel

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Inwentaryzacja Garażu',
+      title: 'Home Storage',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
